5aaa97465677f23d745a88e91610f160bb110cda
Hideki Ogura <b1415988@planet.kanazawa-it.ac.jp>
2017-12-13 08:04:42 +0000

    changed

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -11,17 +11,17 @@ class BuzzerTest(unittest.TestCase):
         self.client = actionlib.SimpleActionClient("music", MusicAction)
         self.device_values = []
 
-	def test_node_exist(self):
-		nodes = rosnode.get_node_names()
-		self.assertIn('/buzzer',nodes, "node does not exist")
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('/buzzer',nodes, "node does not exist")
 
-	def test_put_value(self):
-		pub = rospy.Publisher('/buzzer', UInt16)
-		for i in range(10):
-			pub.publish(1234)
-			time.sleep(0.1)
+    def test_put_value(self):
+        pub = rospy.Publisher('/buzzer', UInt16)
+        for i in range(10):
+            pub.publish(1234)
+            time.sleep(0.1)
 
-        with open("/dev/rtbuzzer0","r") as f:
+       with open("/dev/rtbuzzer0","r") as f:
             data = f.readline()
             self.assertEqual(data,"1234\n","value does not written to rtbuzzer0")
 
@@ -52,6 +52,6 @@ class BuzzerTest(unittest.TestCase):
             self.device_values.append(int(data.rstrip()))
 
 if __name__ == '__main__':
-	time.sleep(3)
-	rospy.init_node('travis_test_buzzer')
-	rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
+    time.sleep(3)
+    rospy.init_node('travis_test_buzzer')
+    rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
